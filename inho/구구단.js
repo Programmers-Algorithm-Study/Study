6229d50071d8d5eb801737468aaa7fe3c929ffc6
duck9-papa <107654769+duck9-papa@users.noreply.github.com>
2022-12-02 23:57:17 +0900

    add

---
+++ inho/구구단.js
@@ -0,0 +1,21 @@
+function googoo(num) {
+    let number = 1;
+  
+    while (number < 4) {
+      
+      for (let i = 1; i < 10; i++) {
+      
+        let result = "";
+        for (let j = num; j < num + 4; j++) {
+          
+          result += `${j} * ${i} = ${j * i}  `;
+        }
+        number+=1
+        console.log(result);
+      }
+    }
+  }
+  
+  googoo(2);
+  console.log(" ");
+  googoo(6);
